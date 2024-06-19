ba39d142b67406afda3cb8a72a60492a3390f30b
Mohamed Mohamed <marvellmoe@gmail.com>
2024-06-19 10:36:29 +0000

    Add Comments model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)9d!x8ki=7oslcnp%vk69y!$6sm-*80vdovu-5h$@yxqxvkz6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-marvellmoe-djangoblog-98mx8lsntui.ws.codeinstitute-ide.net',
