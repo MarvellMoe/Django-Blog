8c0131e2800096f54f94cf64d5e8f40b8643bcd6
Mohamed Mohamed <marvellmoe@gmail.com>
2024-06-20 10:32:39 +0000

    Add Post Detail view for 3rd time

--- blog/views.py
+++ blog/views.py
@@ -25,3 +25,9 @@ def post_detail(request, slug):
         "blog/post_detail.html",
         {"post": post},
     )
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
